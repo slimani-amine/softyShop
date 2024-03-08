598390cc9d90e19005d1daad8debc6aec56a54bf
slimani-amine <slimanimedamine24@gmail.com>
2024-03-08 16:33:21 +0100

    generate some Swagger

--- app/src/v1/presenters/controllers/api/review/createReview.controller.ts
+++ app/src/v1/presenters/controllers/api/review/createReview.controller.ts
@@ -4,21 +4,21 @@ import {
 } from "../../../../usecases/api/review/createReview.usecase";
 import { Request, Response, NextFunction } from "express";
 
-export const createReviewControllerBase = (
-  createReviewUseCase: CreateReviewUseCaseType
-) => async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const result = await createReviewUseCase(req.body);
+export const createReviewControllerBase =
+  (createReviewUseCase: CreateReviewUseCaseType) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    req.body.userId = req.user.id;
+    try {
+      const result = await createReviewUseCase(req.body);
 
-    res.status(201).send({
-      message: "Review created successfully",
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+      res.status(201).send({
+        message: "Review created successfully",
+        data: result,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 
-export const createReviewController = createReviewControllerBase(
-  createReviewUseCase
-);
+export const createReviewController =
+  createReviewControllerBase(createReviewUseCase);
--- app/src/v1/presenters/schemas/review/createProduct.schema.ts
+++ app/src/v1/presenters/schemas/review/createProduct.schema.ts
@@ -7,9 +7,6 @@ const createReviewSchema = z.object({
   rating: z.number().min(0).max(5, {
     message: "The rating should be between 0 and 5",
   }),
-  userId: z.string().min(1, {
-    message: "The user ID is required",
-  }),
   productId: z.string().min(1, {
     message: "The product ID is required",
   }),
